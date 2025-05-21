290a4e0f111a1b2fe769ffb8a66df87746650af2
Mikhail <savchenko.mn.dev@gmail.com>
2025-05-21 17:53:13 -0300

    made homework 1

---
+++ build/CMakeFiles/homework_1.dir/lib.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/homework_1.dir/lib.cpp.o: \
+ /mnt/c/Users/savch/Study/Professional_Cpp/otus_cpp/Homework1/lib.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/savch/Study/Professional_Cpp/otus_cpp/Homework1/lib.h \
+ /mnt/c/Users/savch/Study/Professional_Cpp/otus_cpp/Homework1/build/version.h
---
+++ build/CMakeFiles/homework_cli.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_cli.dir/main.cpp.o"
+  "CMakeFiles/homework_cli.dir/main.cpp.o.d"
+  "homework_cli"
+  "homework_cli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
