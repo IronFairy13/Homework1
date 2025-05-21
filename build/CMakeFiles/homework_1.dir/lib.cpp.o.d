c167705739efdf398161dae96e0808ebde0c15bc
Mikhail <savchenko.mn.dev@gmail.com>
2025-05-21 17:55:00 -0300

    add gitignore

--- build/CMakeFiles/homework_1.dir/lib.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/homework_1.dir/lib.cpp.o: \
- /mnt/c/Users/savch/Study/Professional_Cpp/otus_cpp/Homework1/lib.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/savch/Study/Professional_Cpp/otus_cpp/Homework1/lib.h \
- /mnt/c/Users/savch/Study/Professional_Cpp/otus_cpp/Homework1/build/version.h
